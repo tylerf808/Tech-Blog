c2eea69796febeb4882701e9c93d6fec40fd3f06
Tyler Fabian <tylerf808@gmail.com>
2021-08-05 21:16:44 -0400

    Added seeding functionality

---
+++ seeds/seed.js
@@ -0,0 +1,19 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+const postSeedData = require('./posts.json');
+const userSeedData = require('./users.json')
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userSeedData, { individualHooks: true, returning: true, });
+    const blogPost = await Post.bulkCreate(postSeedData);
+
+    for (const post of blogPost) {
+        await Posts.create({ ...post, user_id: users.id, });
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
